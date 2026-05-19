63fb4709e3d32350c4338d3f9742921be5799556
caioaydrian <caioaydrian42@gmail.com>
2026-05-19 17:40:40 -0300

    mudanças

---
+++ sobre.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="icon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/sobre.css">
+    <title>Lu Fashion Hair</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+                    <img src="logo-certo.png" alt="Logo do salão" class="logo">
+                </li>
+                <li><a href="inicio.php">Home</a></li>
+                <li><a href="servicos.php">Serviços</a></li>
+                <li><a href="sobre.php">Sobre Nós</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1 class="text-center">Equipamentos do nosso Salão</h1>
+        <div class="container text-center">
+            <div class="row row-cols-2">
+                <div class="col">
+                    <a>
+                        <img src="esmaltes.jpg" alt="Esmaltes" class="equipamentos">
+                        <p>Cores de Esmaltes</p>
+                    </a>
+                </div>
+                <div class="col">
+                    <a>
+                        <img src="maquina.jpg" alt="Maquina Blindagem esmalte" class="equipamentos">
+                        <p>Máquina de Blindagem de Esmalte</p>
+                    </a>
+                </div>
+                <div class="col">
+                    <a>
+                        <img src="depilacao.jpg" alt="Cera de depilação" class="equipamentos">
+                        <p>Cera de Depilação</p>
+                    </a>
+                </div>
+                <div class="col">
+                    <a>
+                        <img src="mesa.jpg" alt="Mesa" class="equipamentos">
+                        <p>Cama para depilação</p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
